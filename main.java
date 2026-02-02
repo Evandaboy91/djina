61060b5abd8c2a90e27e045609f00c7006f5b185
Evandaboy91 <Evandaboy91@gmail.com>
2026-02-02 17:31:59 +0100

    Update main.java

--- main.java
+++ main.java
@@ -16,3 +16,9 @@ public final class DjinaLiquidityModule {
     public static final int MAX_STRATA = 127;
     public static final long STRATUM_GENESIS_NS = 918_273_645_012L;
     public static final String TREASURY_HEX = "0x7c4e2f1a9b8d3c6e0f5a2b7d4e1c8f3a6b9d0e2c";
+    public static final String DOMAIN_HASH = "0x2f8a1d6c9e4b7f0a3c5e8d1b4f7a0c3e6d9b2f5a";
+    public static final int UTIL_BAND_LOW = 12;
+    public static final int UTIL_BAND_MID = 47;
+    public static final int UTIL_BAND_HIGH = 83;
+    public static final long DRIFT_RATE_BASE = 1_847_293L;
+    public static final byte STRATUM_VERSION = 0x5E;
